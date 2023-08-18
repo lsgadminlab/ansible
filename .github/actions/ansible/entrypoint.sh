06f38fe4896141d8b8337c1c5d27f0caf4517e40
Luis Carilla Ternero <101694456+lcarilla@users.noreply.github.com>
2023-08-18 18:48:12 +0000

    e

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ls -la
 pwd
-ssh lab@lsglab.ddns.net -i keys/id_rsa "cat /etc/hosts"
+ssh lab@lsglab.ddns.net -i keys/id_rsa -o StrictHostKeyChecking=no "cat /etc/hosts"
 ansible-playbook -vvv playbooks/caddy.yml
