de6bd3726fa193c694c9d68d65a8047088327b55
Luis Carilla Ternero <101694456+lcarilla@users.noreply.github.com>
2023-08-18 18:22:31 +0000

    try

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 ls -la
 pwd
-echo "
-Host bastion
-    HostName lsglab.ddns.net
-    User lab
-    IdentityFile /github/workspace/keys/id_rsa" > .ssh/config
-#ansible-playbook -vvv  playbooks/caddy.yml
+#ansible-playbook -vvv   playbooks/caddy.yml
